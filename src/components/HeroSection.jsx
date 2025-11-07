cc45007a3274fa622df84f251cabd5be71b1abfd
unknown <muktinathrajbanshi786@gmail.com>
2025-11-07 17:10:03 +0200

    now it's perfect

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,13 @@ export const HeroSection = () => {
         <>
             <div className="hero-section main">
                 <div className="container">
-                    
+                    <div className="banner-column">
+                        <div className="banner-inner">
+                            <h1 className="heading-xl">
+                                All Your Files in One Secure Location.
+                            </h1>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
