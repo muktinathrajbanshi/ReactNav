58f5a4da6f68a65dbaf275dc88ecea8e3aa3a086
unknown <muktinathrajbanshi786@gmail.com>
2025-11-08 16:49:11 +0200

    wow navbar section is about to complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,33 @@
 export const Navbar = () => {
-    return <h1>Navbar</h1>;
+    return(
+        <>
+        <header>
+            <div className="container">
+             <div className="grid navbar-grid">
+                <div className="logo">
+                    <h1>Muktinath Tech</h1>
+                </div>
+
+                <nav>
+                    <ul>
+                        <li>
+                         <a href="#">Home</a>
+                        </li>
+                        <li>
+                        <a href="#">About</a>
+                        </li>
+                        <li>
+                        <a href="#">Services</a>
+                        </li>
+                        <li>
+                        <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+
+             </div>
+            </div>
+        </header>
+        </>
+    );
 };
