d271495e3a85e2e643f233cf83e4e0b3e77e72b2
unknown <muktinathrajbanshi786@gmail.com>
2025-11-09 12:09:09 +0400

    adding footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { HeroSection } from './components/HeroSection';
 import { Navbar } from './components/Navbar';
 import "./App.css";
+import { Footer } from './components/Layout/Footer';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,21 @@
+// import { Navlink } from "react-router-dom";
+
+export const Footer = () => {
+    return (
+        <footer className="footer-section">
+        <div className="container">
+            <div className="grid grid-three-cols">
+                <div className="footer-contact">
+                    <div className="icon">
+                        {/* <IoCallSharp /> */}
+                    </div>
+                    <div className="footer-contact-text">
+                        <p>Find us</p>
+                        <p>KTM, Putalisadak</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </footer>
+    );
+};
