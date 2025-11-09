cab7ddf62e70278a3694339a12dd832e5d87d509
unknown <muktinathrajbanshi786@gmail.com>
2025-11-09 12:23:39 +0400

    problem occurred in icons

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,20 +1,38 @@
-// import { Navlink } from "react-router-dom";
+import { IoCallSharp } from "react-icons/io5";
+import { MdPlace } from "react-icons/md";
+import { TbMailPlus } from "react-icons/tb";
+import { NavLink } from "react-router-dom";
+
+import footerContact from "../../api/footerContact.json";
 
 export const Footer = () => {
     return (
         <footer className="footer-section">
-        <div className="container">
+         <div className="container">
             <div className="grid grid-three-cols">
-                <div className="footer-contact">
-                    <div className="icon">
-                        {/* <IoCallSharp /> */}
-                    </div>
-                    <div className="footer-contact-text">
-                        <p>Find us</p>
-                        <p>KTM, Putalisadak</p>
-                    </div>
-                </div>
-            </div>
+
+
+                {
+                    footerContact.map((curData) => {
+                        return (
+                            <>
+                                <div className="footer-contact">
+                                <div className="icon">
+                                    <IoCallSharp />
+                                </div>
+                                <div className="footer-contact-text">
+                                    <p>Find us</p>
+                                    <p>KTM, Putalisadak</p>
+                                </div>
+                               </div>
+                       
+                        </>
+                        )
+                    })
+                }
+
+                
+        </div>
         </div>
         </footer>
     );
